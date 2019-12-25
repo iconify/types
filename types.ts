b244a4950bd8361ce5cb3467b13288d5a570fc08
Vjacheslav Trushkin <cyberalien@gmail.com>
2019-12-25 18:51:07 +0200

    Describe extended JSON format

--- types.ts
+++ types.ts
@@ -1,5 +1,5 @@
 /**
- * Icon dimensions
+ * Icon dimensions.
  *
  * Used in:
  *  icon (as is)
@@ -25,7 +25,7 @@ export interface IconifyDimenisons {
 }
 
 /**
- * Icon transformations
+ * Icon transformations.
  *
  * Used in:
  *  icon (as is)
@@ -49,14 +49,14 @@ export interface IconifyTransformations {
 }
 
 /**
- * Combination of dimensions and transformations
+ * Combination of dimensions and transformations.
  */
 export interface IconifyOptional
 	extends IconifyDimenisons,
 		IconifyTransformations {}
 
 /**
- * Alias
+ * Alias.
  */
 export interface IconifyAlias extends IconifyOptional {
 	// Parent icon index without prefix, required.
@@ -68,7 +68,7 @@ export interface IconifyAlias extends IconifyOptional {
 }
 
 /**
- * Icon
+ * Icon.
  */
 export interface IconifyIcon extends IconifyOptional {
 	// Icon body: <path d="..." />, required.
@@ -79,7 +79,7 @@ export interface IconifyIcon extends IconifyOptional {
 }
 
 /**
- * "icons" field of JSON file
+ * "icons" field of JSON file.
  */
 export interface IconifyIcons {
 	// Index is name of icon, without prefix. Value is IconifyIcon object.
@@ -87,7 +87,7 @@ export interface IconifyIcons {
 }
 
 /**
- * "aliases" field of JSON file
+ * "aliases" field of JSON file.
  */
 export interface IconifyAliases {
 	// Index is name of icon, without prefix. Value is IconifyAlias object.
@@ -95,11 +95,110 @@ export interface IconifyAliases {
 }
 
 /**
- * JSON structure
- *
- * All optional values can exist in root of JSON file, used as defaults
+ * Icon set information block.
  */
-export interface IconifyJSON extends IconifyOptional {
+export interface IconifyInfo {
+	// Icon set name.
+	name: string;
+
+	// Total number of icons.
+	total?: number;
+
+	// Version string.
+	version?: string;
+
+	// Author information.
+	author: {
+		// Author name.
+		name: string;
+
+		// Link to author's website or icon set website.
+		url?: string;
+	};
+
+	// License
+	license: {
+		// Human readable license.
+		title: string;
+
+		// SPDX license identifier.
+		spdx?: string;
+
+		// License URL.
+		url?: string;
+	};
+
+	// Array of icons that should be used for samples in icons list.
+	samples: string[];
+
+	// Icon grid: number or array of numbers.
+	height?: number | number[];
+
+	// Category on Iconify collections list.
+	category?: string;
+
+	// Palette status. True if icons have predefined color scheme, false if icons use currentColor.
+	// Icon set should not mix icons with and without palette to simplify search.
+	palette: boolean;
+}
+
+/**
+ * Optional themes.
+ */
+export interface IconifyThemes {
+	// Key is unique string.
+	[index: string]: {
+		// Theme title.
+		title: string;
+
+		// Icon prefix or suffix, including dash. All icons that start with prefix and end with suffix belong to theme.
+		prefix?: string; // Example: 'baseline-'
+		suffix?: string; // Example: '-filled'
+	};
+}
+
+/**
+ * Characters used in font.
+ */
+export interface IconifyChars {
+	// Index is character, such as "f000".
+	// Value is icon name.
+	[index: string]: string;
+}
+
+/**
+ * Icon categories
+ */
+export interface IconifyCategories {
+	// Index is category title, such as "Weather".
+	// Value is array of icons that belong to that category.
+	// Each icon can belong to multiple categories or no categories.
+	[index: string]: string[];
+}
+
+/**
+ * Meta data stored in JSON file, used for browsing icon set.
+ */
+export interface IconifyMetaData {
+	// Icon set information block. Used for public icon sets, can be skipped for private icon sets.
+	info?: IconifyInfo;
+
+	// Characters used in font. Used for searching by character for icon sets imported from font, exporting icon set to font.
+	chars?: IconifyChars;
+
+	// Categories. Used for filtering icons.
+	categories?: IconifyCategories;
+
+	// Optional themes.
+	themes?: IconifyThemes;
+}
+
+/**
+ * JSON structure.
+ *
+ * All optional values can exist in root of JSON file, used as defaults.
+ */
+export interface IconifyJSON extends IconifyOptional, IconifyMetaData {
 	// Prefix for icons in JSON file, required.
 	prefix: string;
 
